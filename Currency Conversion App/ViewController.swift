2a249dbb68174f7debeb26527641ca6e9c5dd62c
KitaharaMugiro <ngthornym@yahoo.co.jp>
2019-05-23 21:49:42 +0900

    Initial Commit

---
+++ Currency Conversion App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Currency Conversion App
+//
+//  Created by kitaharamugirou on 2019/05/23.
+//  Copyright © 2019 kitaharamugirou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
